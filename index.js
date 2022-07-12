ccea5f8cafa8a4d1ea8ee26c1b1addaf35549f7c
prashantchavan <prashantchavan312199@gmail.com>
2022-07-12 10:09:15 +0530

    basic javascript

---
+++ index.js
@@ -0,0 +1,34 @@
+// this is a basic syntax of javascript 
+
+console.log("hallow world")
+
+
+//  variable 
+// Let const var are use for variable 
+var x=10;
+var y=39;
+var z =x+y;
+console.log(z);
+
+// let 
+// If you think the value of the variable can change, use let.
+
+let message= "hallow i m ";
+let hall=message;
+message="prashant chavan"
+console.log(message);
+
+// const  value not be change 
+    // const fr='apple';
+    // const fr='chiku'
+    // console.log(fr);  gives a error for 
+
+
+    console.log(1/0);
+    // type of 
+
+    console.log(typeof(hall));
+    n=23
+    console.log(typeof(n));
+
+ let fd=prompt(title,[defaul])
