6dbb158489008e761de1ae3b2c183150379cb7a7
prashantchavan <prashantchavan312199@gmail.com>
2022-07-14 13:19:08 +0530

    loops prime number

--- loops.js
+++ loops.js
@@ -42,11 +42,42 @@ for (;k<30;){
 
 // breaking the loop 
 
-let sum=0
-while(true){
-    let value=+prompt("Enter a number ")
+// let sum=0
+// while(true){
+//     let value=+prompt("Enter a number ")
 
-    if(!value) break;
-    sum+=value;
+//     if(!value) break;
+//     sum+=value;
+// }
+// // alert('sum' + sum)
+
+// continue 
+
+for(let i =0;i<10;i++){
+    if(i%1==1) continue;
+    console.log(i);
 }
-alert('sum' + sum)
+
+// replacing for with while 
+
+let l=0;
+while(l<3){
+    console.log(`number is ${l}!`);
+    l++;
+}
+
+
+// let num=prompt('')
+// while(num>100){
+//  let val=prompt("enter the val")
+//   num=num+val
+    
+  
+// }
+// alert('num is gree')
+
+let num;
+do{
+    num=prompt("enter num")
+
+}while(num<=100&&num)
